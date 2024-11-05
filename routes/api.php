c9ecb720731053f6ac32a8b4b29d16c71a168213
GTDevMl <tolentinog945@gmail.com>
2024-11-06 00:38:23 +0800

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ComplaintController;
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeBannerCMSController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,10 +19,6 @@ use App\Http\Controllers\UserController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 // POST METHOD
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/refresh', [AuthController::class, 'refresh']);
@@ -28,17 +26,22 @@ Route::post('/refresh', [AuthController::class, 'refresh']);
 
 Route::post('/book-appointment', [AppointmentController::class, 'create']);
 Route::post('/complaints', [ComplaintController::class, 'create']);
+Route::post('/users', [UserController::class, 'store']); // Create user route
+Route::post('/home-banners', [HomeBannerCMSController::class, 'store']); // Create banner route
 
+// PUT  METHOD
+Route::put('/home-banners/{id}', [HomeBannerCMSController::class, 'update']); // Create banner route
+
+
+
+// DELETE METHOD
 Route::delete('/complaints/{id}', [ComplaintController::class, 'delete']);
-
-Route::delete('/book-appointment/{id}', [ComplaintController::class, 'delete']);
-
-
-
-
-
+Route::delete('/users/{id}', [UserController::class, 'delete']);
+Route::delete('/book-appointment/{id}', [AppointmentController::class, 'delete']);
+Route::delete('/home-banners/{id}', [HomeBannerCMSController::class, 'delete']); // Delete banner route
 
 // GET METHOD
 Route::get('/complaints-list', [ComplaintController::class, 'getcomplaint']);
 Route::get('/booked-appointment', [AppointmentController::class, 'getappointment']);
 Route::get('/users', [UserController::class, 'index']);
+Route::get('/home-banners', [HomeBannerCMSController::class, 'index']); // List banners route
---
+++ storage/framework/views/d82e5f65167200cb500e0d3bab888bcd.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pilar Sorsogon Website</title>
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/js/app.js']); ?>
+  </head>
+  <body>
+    <div id="app"></div>
+  </body>
+</html>
+<?php /**PATH C:\xampp\htdocs\sorsogon_website\resources\views/welcome.blade.php ENDPATH**/ ?>
